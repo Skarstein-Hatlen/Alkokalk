569f1bcf43a263ed86c0290f157bfbecee20a30e
Skarstein-Hatlen <kjetil.skarstein-hatlen@hotmail.no>
2023-06-12 18:25:53 +0200

    reset fix

--- public/javascripts/reset.js
+++ public/javascripts/reset.js
@@ -2,5 +2,8 @@ window.addEventListener('load', function() {
     document.getElementById('resetButton').addEventListener('click', function() {
         document.getElementById('result').innerHTML = '';
         document.getElementById('total').textContent = '';
+        
+        // Reset the totalCalories variable to 0
+        totalCalories = 0;
     });
 });
